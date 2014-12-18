e2bca8cd57f845404e12c4f670977fe9bb2dd2b3
Arthur <208918258@qq.com>
2014-12-18 18:32:29 +0800

    Add Fragement

---
+++ src/co/stayzeal/contacts/MessageFragment.java
@@ -0,0 +1,18 @@
+package co.stayzeal.contacts;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MessageFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.message_fragment, container, false);
+	}
+
+	
+}
