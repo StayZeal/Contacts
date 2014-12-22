2fbafe54a10658d586d738fd3c1ee21f786b5b80
Arthur <208918258@qq.com>
2014-12-22 17:02:36 +0800

    修改了部分包名，和类名

--- src/co/stayzeal/contact/MainActivity.java
+++ src/co/stayzeal/contact/MainActivity.java
@@ -1,6 +1,8 @@
-package co.stayzeal.contacts;
+package co.stayzeal.contact;
 
+import co.stayzeal.contacts.R;
 import android.app.Activity;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,8 +11,19 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+	/**
+	 * 动态添加Fragment主要分为4步：
+	 * 1.获取到FragmentManager，在Activity中可以直接通过getFragmentManager得到。
+	 * 2.开启一个事务，通过调用beginTransaction方法开启。
+	 * 3.向容器内加入Fragment，一般使用replace方法实现，需要传入容器的id和Fragment的实例。
+	 * 4.提交事务，调用commit方法提交。
+	 * getFragmentManager().beginTransaction().replace(R.id.main_layout, fragment1).commit();
+	 */
 	private FragmentManager fragmentManger=getFragmentManager();
-	
+	private Fragment dialFragment=new Fragment();
+	private Fragment historyFragment=new Fragment();
+	private Fragment messageFragment=new Fragment();
+	private Fragment listFragment=new Fragment(); 
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- src/co/stayzeal/contact/MessageFragment.java
+++ src/co/stayzeal/contact/MessageFragment.java
@@ -1,19 +1,20 @@
-package co.stayzeal.contacts;
+package co.stayzeal.contact;
 
+import co.stayzeal.contacts.R;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HistoryMessage extends Fragment {
+ 
+
+public class MessageFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		
-		return inflater.inflate(R.layout.history_fragment, container, false);
-		
+		return inflater.inflate(R.layout.message_fragment, container, false);
 	}
 
 	
