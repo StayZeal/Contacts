78dea2d82e2847ba88c9979574c108c7a8f46222
Arthur <208918258@qq.com>
2014-12-26 10:33:45 +0800

    lala

---
+++ src/co/stayzeal/contact/ContactInfoActivity.java
@@ -0,0 +1,16 @@
+package co.stayzeal.contact;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContactInfoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contact_info);
+	}
+
+	
+}
